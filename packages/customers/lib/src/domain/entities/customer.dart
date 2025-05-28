c932280b953ca74a14c03c8a0a8714077a1403ca
izerokim <kimgichuru6@gmail.com>
2025-05-28 23:11:48 +0300

    Create customer entity

---
+++ packages/customers/lib/src/domain/entities/customer.dart
@@ -0,0 +1,11 @@
+class Customer {
+  final int id;
+  final String name;
+  final DateTime createdAt;
+
+  Customer({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+}
